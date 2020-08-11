87de7482bd06dbcb52a5311e6b671f8188a29f3e
BrandHernandez <brandon32145@gmail.com>
2020-08-10 20:38:36 -0500

    Create iniciarPagina.js

---
+++ js/iniciarPagina.js
@@ -0,0 +1,19 @@
+
+var mostrarImagen = document.getElementById("mostrarImagen"),
+    mostrarImagen2 = document.getElementById("mostrarImagen2"),
+    imagenFinal = document.getElementById("imagenFinal"),        
+    nombreColor = document.getElementById("nombreColor"),
+    colextF = document.getElementById("colextF"),
+    nombreCamara = document.getElementById("nombreCamara"),
+    aspersores = document.getElementById("aspersores"),
+    camaraFinal = document.getElementById("camaraFinal");
+    
+    mostrarImagen.innerHTML= "<img src='img/Ceres/Camara1/Ceres_4_aspersores/Blanco/blanco.jpg' class='magnify-image img-fluid mx-auto d-block' data-magnify-src='img/Ceres/Camara1/Ceres_4_aspersores/Blanco/blanco_largo.jpg'>";
+    mostrarImagen2.innerHTML="<img src='img/Ceres/Camara1/Ceres_4_aspersores/Blanco/blanco.jpg' class='magnify-image img-fluid mx-auto d-block' data-magnify-src='img/Ceres/Camara1/Ceres_4_aspersores/Blanco/blanco_largo.jpg'>";
+    imagenFinal.innerHTML = "<img src='img/Ceres/Camara1/Ceres_4_aspersores/Blanco/blanco.jpg' class='img-fluid mx-auto d-block'>";        
+    nombreColor.innerHTML = "<p class='p d-inline ml-3'>Blanco</p><p class='p pcole d-inline ml-5'>Incluido</p> ";
+    nombreCamara.innerHTML = "<p class='p d-inline ml-5'>Camara Tipo 1</p><p class='p pcole d-inline ml-5'>Incluido</p> ";
+    precio.innerHTML = "<p class='pre d-inline'>$15,000</p> ";
+    colextF.innerHTML = "<p class='p d-inline'> Blanco</p> ";
+    aspersores.innerHTML = "<p class='p d-inline'> 4 </p> ";
+    camaraFinal.innerHTML = "<p class='p d-inline'> Tipo 1 </p> ";
